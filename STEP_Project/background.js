89145d3abb93741c36344fb410f6cc558062acc0
Rajeshwari <raji.jadhav6@gmail.com>
2020-06-29 12:40:25 +0530

    Popup functionality added

--- STEP_Project/background.js
+++ STEP_Project/background.js
@@ -5,7 +5,8 @@ chrome.runtime.onInstalled.addListener(function()
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'admin.google.com'},
+          //regex expression matches settings or OU page
+          pageUrl: {urlMatches: '.*admin.google.com\/u\/[0-9]+\/ac\/(appsettings)|(orgunits)|(settings/serviceonoff)'}
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
