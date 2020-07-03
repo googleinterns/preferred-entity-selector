1eba2ec6af96bf841d43d6e3976000fcc6c68d74
Varun Sivashankar <varunsiva33@gmail.com>
2020-07-03 13:13:52 +0530

    added functions to OUListContent.js with tests

--- STEP_Project/OUListContent.js
+++ STEP_Project/OUListContent.js
@@ -8,6 +8,27 @@ function addRemoveButtonClick (event)
 }
 
 /**
+ * Adds a button to every visible row when the page is first loaded.
+ * @param {HTML Object} tabl - contains OU rows
+ */
+function initButtons(tabl)
+{
+    let OUs = tabl.rows;
+    let numOUs = OUs.length;
+    let numRows = numOUs-1;
+    addButtons(OUs,numRows);
+
+    /**
+     * This is to handle an edge case:
+     * It deals with the last row in the table that doesn't actually correspond to an OU
+     */
+    let row = OUs[numOUs-1];
+    const button = document.createElement('p');
+    button.setAttribute('class','bClass');
+    row.appendChild(button);
+}
+
+/**
  * This adds a button to every row in the table
  * If the row already has a button, it skips that row
  * @param {HTML Object} OUs - List of OUs
@@ -31,9 +52,40 @@ function addButtons(OUs, numRows)
     }
 }
 
+/**
+ * This function implements Mutation Observer
+ * It checks if new OUs are visible and adds buttons to new rows if required
+ */
+function monitorChanges()
+{
+    let allRowNodes = document.querySelectorAll("tbody[role=rowgroup]");
+    let rootNode = allRowNodes[0];
+
+    let MutationObserver = window.MutationObserver;
+
+    let observer = new MutationObserver(function(mutations) 
+    {
+        let tabl = document.querySelector('table[role=grid]');
+        let OUs = tabl.rows;
+        let numOUs = OUs.length;
+
+        addButtons(OUs,numOUs);
+    });
+
+    observer.observe(rootNode, 
+    {
+        attributes: false, 
+        childList: true, 
+        characterData: false,
+        subtree: true
+    });
+}
+
 (function()
 {
     let tabl = document.querySelector('table[role=grid]');
     tabl.addEventListener('click',addRemoveButtonClick); //event delegation
+    initButtons(tabl);
+    monitorChanges();
 }()
 );
--- STEP_Project/tests/OUListContentTest.js
+++ STEP_Project/tests/OUListContentTest.js
@@ -2,6 +2,30 @@ let tabl;
 let OUs;
 let numRows;
 
+describe('Testing the initButtons function', ()=>{
+    beforeEach(function()
+    {
+        tabl = document.querySelector('table[role=grid]');
+        OUs = tabl.rows;
+    })
+
+    it('Should not add a button to the first row', ()=>{
+        initButtons(tabl);
+        let row = OUs[0];
+        let temp = row.getElementsByClassName('bClass');
+        expect(temp.length).toEqual(0);
+    })
+
+    it('Should add a button to every row after that', ()=>{
+        let row = OUs[1];
+        let temp = row.getElementsByClassName('bClass');
+        expect(temp.length).toEqual(1);
+        row = OUs[2];
+        temp = row.getElementsByClassName('bClass');
+        expect(temp.length).toEqual(1);
+    })
+})
+
 describe('Testing the addButtons function', ()=>{
     beforeEach(function()
     {
@@ -68,7 +92,44 @@ describe('Testing the addButtons function', ()=>{
         row = OUs[2];
         temp = row.getElementsByClassName('bClass');
         expect(temp.length).toEqual(1); 
+    })
+}) 
 
+describe('Testing monitorChanges function (mutation observer)', ()=>{
+    beforeEach(function()
+    {
+        tabl = document.querySelector('table[role=grid]');
+        OUs = tabl.rows;
+        numRows = OUs.length;
+    })
+
+    it('Should add a button to a new row automatically', ()=>{
+        monitorChanges();
+        tabl.insertRow();
+        let numRows = OUs.length;
+        expect(numRows).toEqual(4);
+
+        //setTimeout required due to asynchronous behaviour 
+        setTimeout(function()
+        { 
+                let row = OUs[3];
+                let temp = row.getElementsByClassName('bClass');
+                expect(temp.length).toEqual(1);
+        }, 3000);      
+    })
+
+    it('Should not result in 0 buttons in any row (except row 0) or more than 1 button in any row', ()=>{
+
+        let row = OUs[0];
+        let temp = row.getElementsByClassName('bClass');
+        expect(temp.length).toEqual(0);
+
+        for (let i = 1; i < numRows-1; i++)
+        {
+            let row = OUs[i];
+            let temp = row.getElementsByClassName('bClass');
+            expect(temp.length).toEqual(1);
+        }
         tabl.remove();
     })
 }) 
