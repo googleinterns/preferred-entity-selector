586e9aa9b86b6662ee41640bda5fd2caa51fa6dd
Varun Sivashankar <varunsiva33@gmail.com>
2020-07-08 14:23:35 +0530

    changed variable names

--- STEP_Project/OUSelectContent.js
+++ STEP_Project/OUSelectContent.js
@@ -7,14 +7,14 @@ function selectClick ()
     chrome.storage.sync.get(null, function (data) 
     {
         let tabl = document.querySelector('ul[role=group]');
-        let OUs = tabl.getElementsByTagName("li");
-        let numOUs = OUs.length;
+        let orgUnits = tabl.getElementsByTagName("li");
+        let numOrgUnits = orgUnits.length;
 
-        for (let i = 0; i < numOUs; i++)
+        for (let i = 0; i < numOrgUnits; i++)
         {
-            let row = OUs[i];
-            var dataid = row.children[0].getAttribute("data-node-id");
-            var dataname = (row.innerText).replace(/[^\x20-\x7E]/g, ''); //remove stray non-ASCII characters
+            let row = orgUnits[i];
+            let dataid = row.children[0].getAttribute("data-node-id");
+            let dataname = (row.innerText).replace(/[^\x20-\x7E]/g, ''); //remove stray non-ASCII characters
 
             if (data[dataid] != undefined)
             {
